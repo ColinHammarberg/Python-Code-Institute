798fb03bd049ba9358c93ac97379682d4d9fb82d
ColinHammarberg <colin.hammarberg2@gmail.com>
2021-06-24 13:10:17 +0000

    Worked on the contact form according to the instructions and lessons

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 import os
 import json
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 
 app = Flask(__name__)
@@ -24,14 +24,19 @@ def about_member(member_name):
     with open("data/company.json", "r") as json_data:
         data = json.load(json_data)
         for obj in data:
-            if obj["url"] == member_name: 
+            if obj["url"] == member_name:
                 member = obj
     return render_template("member.html", member=member)
 
 
 
-@app.route("/contact")
+@app.route("/contact", methods=["GET", "POST"])
 def contact():
+    if request.method == "POST":
+        print(request.form.get("name"))
+        print(request.form["email"])
+        print(request.form["phone"])
+        print(request.form["message"])
     return render_template("contact.html", page_title="Contact")
 
 
